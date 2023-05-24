636fc715d7f3c3bdde599d1455d797a8c600098a
MuhammadArsalanNaeem2004 <130148088+MuhammadArsalanNaeem2004@users.noreply.github.com>
2023-05-24 17:34:52 +0500

    Add files via upload

---
+++ facebok/app.js
@@ -0,0 +1,23 @@
+var inp1 = document.getElementById("firstname");
+var disp1 = document.getElementById("display1");
+
+var inp2 = document.getElementById("surname");
+var disp2 = document.getElementById("display2");
+
+var inp3 = document.getElementById("password");
+var disp3 = document.getElementById("display3");
+
+var inp4 = document.getElementById("age");
+var disp4 = document.getElementById("display4");
+
+var inp5 = document.getElementById("gender");
+var disp5 = document.getElementById("display5");
+
+function register() {
+    console.log(inp1.value);
+    console.log(inp2.value);
+    console.log(inp3.value);
+    console.log(inp4.value);
+    console.log(inp5.value);
+}
+
